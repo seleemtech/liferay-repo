e057b302d3c2529c2faa1b4494d932752d940e1b
mahammad seleem <seleem@esqaureinfo.com>
2023-05-26 15:12:11 +0530

    'dsd'

---
+++ modules/te/src/main/java/te/constants/TePortletKeys.java
@@ -0,0 +1,11 @@
+package te.constants;
+
+/**
+ * @author DELL
+ */
+public class TePortletKeys {
+
+	public static final String TE =
+		"te_TePortlet";
+
+}
---
+++ modules/te/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="te.caption"/></b>
+</p>
