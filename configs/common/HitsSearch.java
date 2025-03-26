bbd0f8030d6be00f4a59566d08e492f17f80e11a
Mahammed Seleem <104485911+seleemtech@users.noreply.github.com>
2025-03-26 15:43:38 +0530

    Update HitsSearch.java

--- configs/common/HitsSearch.java
+++ configs/common/HitsSearch.java
@@ -71,3 +71,111 @@ public List<Document> search(long companyId, String keywords) throws SearchExcep
 
 			return documentsList;
 	}
+
+
+package com.ews.search.web.command;
+
+import com.ews.bus.sl.model.Bus;
+import com.ews.bus.sl.service.BusLocalServiceUtil;
+import com.ews.search.web.constants.SearchWebPortletKeys;
+import com.ews.search.web.dto.ResultDTO;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(immediate = true, property = { "javax.portlet.name=" + SearchWebPortletKeys.SEARCHRESULT,
+		"mvc.command.name=/" }, service = MVCRenderCommand.class)
+public class DefaultSearchResultsMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException {
+		ThemeDisplay themeDisplay = (ThemeDisplay) renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+		String keyword = ServiceContextThreadLocal.getServiceContext().getRequest().getParameter("keyword");
+		_log.info("keyword >>>>>>>>>>>>>>>." + keyword);
+		List<ResultDTO> resultDTOs = new LinkedList<>();
+		if (Validator.isNotNull(keyword)) {
+			try {
+				List<Document> resultsDocs = BusLocalServiceUtil.search(themeDisplay.getCompanyId(), keyword);
+
+				_log.info("resultsDocs >>>>>>>>>>>>>>>." + resultsDocs.size());
+				_log.info("main class name " + Bus.class.getName());
+				resultsDocs.stream().forEach(doc -> {
+					ResultDTO resultDTO = new ResultDTO();
+					if (doc.get("entryClassName").equals(Bus.class.getName())) {
+						resultDTO = setBusResult(Long.valueOf(doc.get("entryClassPK")));
+						resultDTOs.add(resultDTO);
+					} else if (doc.get("entryClassName").equals(User.class.getName())) {
+						resultDTO = setUserResult(Long.valueOf(doc.get("entryClassPK")));
+						resultDTOs.add(resultDTO);
+					}
+
+				});
+			} catch (SearchException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		renderRequest.setAttribute("resultDTOs", resultDTOs);
+		return "/search-results/view.jsp";
+	}
+
+	private ResultDTO setUserResult(long primaryKey) {
+		ResultDTO resultDTO = new ResultDTO();
+		User user = UserLocalServiceUtil.fetchUser(primaryKey);
+
+		if (Validator.isNull(user)) {
+			return resultDTO;
+		}
+
+		resultDTO.setPrimaryKey(user.getUserId());
+		resultDTO.setModel("User");
+		resultDTO.setName(user.getFullName());
+		resultDTO.setCreatedBy(getCreatedByUserName(user.getUserId()));
+		return resultDTO;
+
+	}
+
+	private ResultDTO setBusResult(long primaryKey) {
+		ResultDTO resultDTO = new ResultDTO();
+		Bus bus = BusLocalServiceUtil.fetchBus(primaryKey);
+
+		if (Validator.isNull(bus)) {
+			return resultDTO;
+		}
+
+		resultDTO.setPrimaryKey(bus.getPrimaryKey());
+		resultDTO.setModel("Bus");
+		resultDTO.setName(bus.getName());
+		resultDTO.setType(bus.getType());
+		resultDTO.setCreatedBy(getCreatedByUserName(bus.getUserId()));
+		return resultDTO;
+
+	}
+
+	private String getCreatedByUserName(long primaryKey) {
+		User user = UserLocalServiceUtil.fetchUser(primaryKey);
+		return user != null ? user.getFullName() : StringPool.BLANK;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(DefaultSearchResultsMVCRenderCommand.class);
+}
+
